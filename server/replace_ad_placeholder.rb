19ef10be0e477a8d740eb7ecb9618615dd684ced
Chris Malone <chris@m11design.com>
2025-04-29 07:59:05 -0400

    Update replace_ad_placeholder.rb

--- server/replace_ad_placeholder.rb
+++ server/replace_ad_placeholder.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 after_initialize do
-  # Extend Email::MessageBuilder to inject our ad_placeholder variable
-  Email::MessageBuilder.class_eval do
-    alias_method :original_build_custom_variables, :build_custom_variables
-
-    def build_custom_variables
-      vars = original_build_custom_variables
-      vars[:ad_placeholder] = "" # Dummy value so the validator allows %{ad_placeholder}
-      vars
-    end
-  end
-
-  # Still override replace_variables to actually swap the placeholder with real ad HTML
   Email::Renderer.class_eval do
     alias_method :original_replace_variables, :replace_variables
 
@@ -29,7 +17,7 @@ after_initialize do
         </p>
       HTML
 
-      result.gsub('%{ad_placeholder}', ad_content)
+      result.gsub('<!-- ad_placeholder -->', ad_content)
     end
   end
 end
