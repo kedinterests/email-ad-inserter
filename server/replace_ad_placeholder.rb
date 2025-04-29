293d338155c8bdffe15c4971bd29a47552e541fd
Chris Malone <chris@m11design.com>
2025-04-29 07:56:31 -0400

    Update replace_ad_placeholder.rb

--- server/replace_ad_placeholder.rb
+++ server/replace_ad_placeholder.rb
@@ -5,9 +5,13 @@ after_initialize do
     alias_method :original_replace_variables, :replace_variables
 
     def replace_variables(text, opts)
+      # Define a dummy ad_placeholder so Discourse doesn't complain
+      opts ||= {}
+      opts[:ad_placeholder] ||= ""
+
       result = original_replace_variables(text, opts)
 
-      # 🟢 Hardcoded Ad Content
+      # Now do our replacement manually
       ad_content = <<~HTML
         <hr>
         <p style="font-size: 14px; color: #555; text-align: center;">
@@ -18,7 +22,6 @@ after_initialize do
         </p>
       HTML
 
-      # Replace the magic tag
       result.gsub('%{ad_placeholder}', ad_content)
     end
   end
