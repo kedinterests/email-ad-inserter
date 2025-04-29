7c67d44298c2411d0316387483c9f0c26712ddb4
Chris Malone <chris@m11design.com>
2025-04-29 07:52:22 -0400

    Update replace_ad_placeholder.rb

--- server/replace_ad_placeholder.rb
+++ server/replace_ad_placeholder.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
-# This runs after Discourse initializes
 after_initialize do
-  # Patch Email::Renderer to replace %{ad_placeholder} with the theme setting value
   Email::Renderer.class_eval do
-	alias_method :original_replace_variables, :replace_variables
+    alias_method :original_replace_variables, :replace_variables
 
-	def replace_variables(text, opts)
-	  result = original_replace_variables(text, opts)
-	  ad_content = ThemeModifier.get_setting(:ad_placeholder_content).to_s
-	  result.gsub('%{ad_placeholder}', ad_content)
-	end
+    def replace_variables(text, opts)
+      result = original_replace_variables(text, opts)
+
+      # 🟢 Hardcoded Ad Content
+      ad_content = <<~HTML
+        <hr>
+        <p style="font-size: 14px; color: #555; text-align: center;">
+          🚀 Check out our latest guide:
+          <a href="https://yourwebsite.com/awesome-guide" style="color: #0077cc; text-decoration: underline;">
+            5 Ways to Boost Your Forum Engagement
+          </a>
+        </p>
+      HTML
+
+      # Replace the magic tag
+      result.gsub('%{ad_placeholder}', ad_content)
+    end
   end
 end
